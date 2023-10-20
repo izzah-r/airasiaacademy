a49e7131933fd9cd56f5506dd0101145e3423f3e
izzah-r <148434454+izzah-r@users.noreply.github.com>
2023-10-20 13:33:21 +0800

    Add files via upload

--- project.py
+++ project.py
@@ -1,9 +1,6 @@
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
 import streamlit as st
+import pandas as pd
 import pickle
 
 from google.colab import drive
