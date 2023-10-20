4be8bc5e52ff254e911a8110c89112eb5c976d34
izzah-r <148434454+izzah-r@users.noreply.github.com>
2023-10-20 13:27:18 +0800

    Add files via upload

--- project.py
+++ project.py
@@ -22,14 +22,14 @@ This app predicts the **advertising** types!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 4.3, 7.9, 5.4)
-    Radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
-    Newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
-    Sale = st.sidebar.slider('Sales', 0.1, 2.5, 0.2)
+    TV = st.sidebar.slider('TV', 16.0, 100.0, 230.0)
+    Radio = st.sidebar.slider('Radio', 10.0, 20.4, 45.9)
+    Newspaper = st.sidebar.slider('Newspaper', 45.1, 50.3, 69.3)
+    Sale = st.sidebar.slider('Sales', 9.3, 2.5, 22.1)
     data = {'TV': TV,
             'Radio': Radio,
-            'petal_length': petal_length,
-            'Newspaper': Newspaper}
+            'Newspaper': Newspaper,
+            'Sales': Sales}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -38,7 +38,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = pickle.load(open("Advertising.h5", "wb"))
+loaded_model = pickle.load(open("Advertising.h5", "rb"))
 
 prediction = loaded_model.predict(df)
 
